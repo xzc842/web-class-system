a4f09b9bafe4ea495ef2c04f8fc8ab555fe40fc4
Zi Cheng Xue <91328323+xzc842@users.noreply.github.com>
2022-03-31 13:44:34 +0800

    Add files via upload

---
+++ upuser/anquancenter/resetpass.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>找回密码</title>
+        <meta charset="utf-8">
+    </head>
+    <body bgcolor="#1E90FF">
+        <div style="margin-top:0.5cm;margin-left:0.5cm;background-color:#FFD700;height:50px;width:1300px;">
+            <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;安全中心</h1>
+        </div>
+        <div style="margin-top:0.5cm;margin-left:1.5cm;height:600px;width:1000px;background-color:#DAA520">
+            <a href="resetpass/email.html">邮箱找回密码</a>
+        </div>
+    </body>
+</html>
